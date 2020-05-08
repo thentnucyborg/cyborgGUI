7cc0eda4d3a6df53cc5f721edf5ead938743335a
paytience <caspern@stud.ntnu.no>
2020-05-08 17:43:16 +0200

    ariastatus and debugconsole

--- src/components/ariaStatus.vue
+++ src/components/ariaStatus.vue
@@ -1,106 +1,102 @@
 <template>
   <div id="ariaStatus">
     <b-row align-h="center">
-        hello
+      <b-card class="m-2" header="Current state">
+        <b-card-text>{{ current_state }}</b-card-text>
+      </b-card>
     </b-row>
     <b-row align-h="center">
-       hallo
+      <b-card class="m-2" header="Prev State">
+        <b-card-text>{{ previous_state }}</b-card-text>
+      </b-card>
+      <b-card class="m-2" header="Event">
+        <b-card-text>{{ event }}</b-card-text>
+      </b-card>
     </b-row>
   </div>
 </template>
 
 <script>
-import ROSLIB from 'roslib';
-
-// function getTopics() {
-//   var cmdBehaviour = new ROSLIB.Topic({
-//     ros: this.$ros,
-//     name: "/cyborg_commander/robot_mode",
-//     messageType: "std_msgs/String"
-//   });
-//   var behaviourVal = new ROSLIB.Message({
-//     data: val
-//   });
-//   cmdBehaviour.publish(behaviourVal);
-// }
+import ROSLIB from "roslib";
 
 export default {
   name: "modeButtons",
-  methods: {
-    // getTopics
+  data() {
+    return {
+      previous_state: "-",
+      event: "-",
+      current_state: "-"
+    };
   },
-  mounted() {
-      var systemStateTopic = new ROSLIB.Topic({
-        ros: this.$ros,
-        name: '/cyborg_controller/state_change',
-        messageType: 'cyborg_controller/SystemState'
+  created() {
+    var self = this;
+    this.systemStateTopic = new ROSLIB.Topic({
+      ros: this.$ros,
+      name: "/cyborg_controller/state_change",
+      messageType: "cyborg_controller/SystemState"
     });
 
-    systemStateTopic.subscribe(function (msg) {
-        console.log("got data"+ String(msg.event));
-        // document.getElementById("txtSMACHEvent").innerHTML = msg.event;
-        // document.getElementById("txtPrevSMACHState").innerHTML = msg.from_system_state;
-        // document.getElementById("txtCurrSMACHState").innerHTML = msg.to_system_state;
+    this.systemStateTopic.subscribe(function(msg) {
+      console.log("got data1" + String(msg.event));
+      self.previus_state = msg.event;
+      self.event = msg.event;
+      self.current_state = msg.to_system_state;
+      // document.getElementById("txtSMACHEvent").innerHTML = msg.event;
+      // document.getElementById("txtPrevSMACHState").innerHTML = msg.from_system_state;
+      // document.getElementById("txtCurrSMACHState").innerHTML = msg.to_system_state;
     });
 
-    var emotionalStateTopic = new ROSLIB.Topic({
-        ros: this.$ros,
-        name: '/cyborg_controller/emotional_state',
-        messageType: 'cyborg_controller/EmotionalState'
+    this.emotionalStateTopic = new ROSLIB.Topic({
+      ros: this.$ros,
+      name: "/cyborg_controller/emotional_state",
+      messageType: "cyborg_controller/EmotionalState"
     });
 
-    emotionalStateTopic.subscribe(function (msg) {
-        console.log("got data" + String(msg));
-        // document.getElementById("txtPrevEmotionalState").innerHTML = msg.from_emotional_state;
-        // document.getElementById("txtCurrEmotionalState").innerHTML = msg.to_emotional_state;
-        // document.getElementById("txtPleasure").innerHTML = msg.current_pleasure.toFixed(4);
-        // document.getElementById("txtArousal").innerHTML = msg.current_arousal.toFixed(4);
-        // document.getElementById("txtDominance").innerHTML = msg.current_dominance.toFixed(4);
+    this.emotionalStateTopic.subscribe(function(msg) {
+      console.log("got data2" + String(msg));
+      // document.getElementById("txtPrevEmotionalState").innerHTML = msg.from_emotional_state;
+      // document.getElementById("txtCurrEmotionalState").innerHTML = msg.to_emotional_state;
+      // document.getElementById("txtPleasure").innerHTML = msg.current_pleasure.toFixed(4);
+      // document.getElementById("txtArousal").innerHTML = msg.current_arousal.toFixed(4);
+      // document.getElementById("txtDominance").innerHTML = msg.current_dominance.toFixed(4);
     });
 
-    var motorStateTopic = new ROSLIB.Topic({
-        ros: this.$ros,
-        name: '/RosAria/motors_state',
-        messageType: 'std_msgs/Bool'
+    this.motorStateTopic = new ROSLIB.Topic({
+      ros: this.$ros,
+      name: "/RosAria/motors_state",
+      messageType: "std_msgs/Bool"
     });
 
-    motorStateTopic.subscribe(function (msg) {
-        if (msg.data == true){
-            console.log("got data"  + String(msg));
-            // document.getElementById("txtMotorsState").innerHTML = "ON";
-        }else{
-            console.log("got data"  + String(msg));
-            // document.getElementById("txtMotorsState").innerHTML = "OFF";
-        } 
+    this.motorStateTopic.subscribe(function(msg) {
+      if (msg.data == true) {
+        console.log("got data3" + String(msg));
+        // document.getElementById("txtMotorsState").innerHTML = "ON";
+      } else {
+        console.log("got data3" + String(msg));
+        // document.getElementById("txtMotorsState").innerHTML = "OFF";
+      }
     });
 
-    var behaviourStateTopic = new ROSLIB.Topic({
-        ros: this.$ros,
-        name: '/cyborg_commander/robot_mode',
-        messageType: 'std_msgs/Bool'
+    this.behaviourStateTopic = new ROSLIB.Topic({
+      ros: this.$ros,
+      name: "/cyborg_commander/robot_mode",
+      messageType: "std_msgs/String"
     });
 
-    behaviourStateTopic.subscribe(function (msg) {
-        if (msg.data == true){
-            console.log("got data"  + String(msg));
-            // document.getElementById("txtBehaviourState").innerHTML = "ON";
-        }else{
-            console.log("got data"  + String(msg));
-            // document.getElementById("txtBehaviourState").innerHTML = "OFF";
-        } 
+    this.behaviourStateTopic.subscribe(function(msg) {
+      if (msg.data == true) {
+        console.log("got data4" + String(msg));
+        // document.getElementById("txtBehaviourState").innerHTML = "ON";
+      } else {
+        console.log("got data4" + String(msg));
+        // document.getElementById("txtBehaviourState").innerHTML = "OFF";
+      }
     });
-
-    var rosOutTopic = new ROSLIB.Topic({
-        ros: this.$ros,
-        name: '/rosout',
-        messageType: 'rosgraph_msgs/Log'
-    });
-    var textArea = document.getElementById("formControlTextarea1");
-    rosOutTopic.subscribe(function (msg) {
-        console.log("got data"  + String(msg));
-        document.getElementById("formControlTextarea1").innerHTML += msg.msg + "\n";
-        textArea.scrollTop = textArea.scrollHeight;
-    });
+  },
+  destroyed() {
+    this.motorStateTopic.unsubscribe();
+    this.behaviourStateTopic.unsubscribe();
+    this.systemStateTopic.unsubscribe();
   }
 };
 </script>
--- src/components/debugConsole.vue
+++ src/components/debugConsole.vue
@@ -1,18 +1,46 @@
 <template>
-    <div id="debugConsole">
-        <b-form-group>
-            <label>Debug Output</label>
-            <b-form-textarea class="form-control" id="formControlTextarea1" rows="16" spellcheck="false" readonly disabled style="margin-top: 0px; margin-bottom: 0px;"></b-form-textarea>
-        </b-form-group>
-    </div>
+  <div id="debugConsole">
+    <b-form-group>
+      <label>Debug Output</label>
+      <b-form-textarea
+        class="form-control"
+        id="formControlTextarea1"
+        rows="16"
+        spellcheck="false"
+        readonly
+        disabled
+        style="margin-top: 0px; margin-bottom: 0px;"
+      ></b-form-textarea>
+    </b-form-group>
+  </div>
 </template>
 
 <script>
+import ROSLIB from "roslib";
 export default {
-    name: "debugConsole"
-}
+  name: "debugConsole",
+  created() {
+    this.rosOutTopic = new ROSLIB.Topic({
+      ros: this.$ros,
+      name: "/rosout",
+      messageType: "rosgraph_msgs/Log"
+    });
+    this.rosOutTopic.subscribe(function(msg) {
+      console.log("got data6" + String(msg));
+      var textArea = document.getElementById("formControlTextarea1");
+      textArea.innerHTML +=
+        msg.msg + "\n";
+    textArea.scrollTop = textArea.scrollHeight;
+    });
+  },
+  mounted(){
+
+  },
+  destroyed(){
+      this.rosOutTopic.unsubscribe();
+  }
+};
 </script>
 
 <style>
-
 </style>
